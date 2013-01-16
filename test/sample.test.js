96351478adbf1a543482152655fbf56051df8011
labajo <jherlab@gmail.com>
2013-01-16 09:27:24 +0100

    Add sample test.

---
+++ test/sample.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Sample Test', function(){
+    describe('My Sample Test', function(){
+        it('check Asserts', function(){
+            assert.equal(true,true);
+
+        })
+    })
+})
